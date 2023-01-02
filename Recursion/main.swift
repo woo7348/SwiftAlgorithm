4d4414fab0d2a55ff7e2b0908884855b314fc46f
MinWoo <mynooo7688@gmail.com>
2023-01-02 16:48:18 +0900

    스위프트 알고리즘

--- Recursion/main.swift
+++ Recursion/main.swift
@@ -1,17 +1,17 @@
 //
 //  main.swift
-//  SwiftAlgorithm
+//  Recursion
 //
 //  Created by 강민우 on 2023/01/02.
 //
-
 import Foundation
 
 func factorial(_ num: Int) -> Int {
     var result = 1
     for i in 2...num {
         result *= i
-        print(i)
     }
+    print(i)
     return result
 }
+
